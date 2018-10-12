07c5d0354ccb796f0f3b681c267373ddb6ca4f01
Luis <vvegem@gmail.com>
2018-10-12 15:58:49 -0300

    Function Substract
    Se agregó la función que resta los valores entregados por la URL

---
+++ math_operations.js
@@ -0,0 +1,61 @@
+/**
+ ** Add method
+ ** This method takes the query parameter from the url and response stream, in order to go throw the params
+ ** and make the add operation, and to send the response on the response stream.
+ **/
+function add (query, response) {
+  var suma = 0
+  for (var propName in query) {
+      var splitted = query[propName]
+      if(isNumeric(splitted)) {
+        //Ok, it's a number
+        var number = Number(splitted)
+        suma += number
+      } else {
+        //It's not a number, so we show an error message and sends to the client
+        response.writeHead(400, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+      }
+  }
+  //If everithing was OK, we send code 200 response, using json format
+  response.writeHead(200, {'Content-Type': 'application/json'})
+  response.end(JSON.stringify({result:suma}))
+}
+
+function substract (query, response) {
+  var resta = 0;
+  var first_n = 0;
+  var c = 0;
+  for (var propName in query) {
+    var splitted = query[propName];
+    if(isNumeric(splitted) && c === 0) {
+      var number = Number(splitted);
+      first_n = number;
+      c++;
+    } 
+    else if (isNumeric(splitted) && c != 0) {
+      var number = Number(splitted);
+      resta -= number;
+    }
+    else{
+      response.writeHead(400, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify({result:'error, el input debe ser numerico'}));
+    }
+  }
+  resta += first_n;
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.end(JSON.stringify({result:resta}));
+}
+
+/**
+ ** isNumeric returns true if input is a number. This can be done using the oppsite behaviour from isNaN function
+ **/
+
+function isNumeric(num) {
+  //isNaN returns false if the input is a number, true otherwise
+  return !isNaN(num)
+}
+
+//In this line we export the functions to be used in other javascript files. We must import this module with 'require' signature
+module.exports.add = add
+module.exports.substract = substract;
--- node_example.js
+++ node_example.js
@@ -1,36 +1,19 @@
-var http = require('http')
+const http = require('http')
+const url = require('url')
+const math_ops = require('./math_operations')
 
 var server = http.createServer(function (request, response) {
-  var url = request.url
-  var data = url.split('?')
+  const parsedUrl = url.parse(request.url, true)
 
-  if(data[0] === '/suma') {
-    var suma = 0
-    var input = data[1].split('&')
-    input.forEach(function(numero) {
-      var splitted = numero.split('=')[1]
-      if(isNumeric(splitted)) {
-        //Ok, it's a number
-        var number = Number(splitted)
-        suma += number
-      } else {
-        //It's not a number, so we show an error message and sends to the client
-        response.writeHead(400, {'Content-Type': 'application/json'})
-        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
-      }
-    })
-    //If everithing was OK, we send code 200 response, using json format
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.end(JSON.stringify({result:suma}))
+  if(parsedUrl.pathname === '/suma') {
+    math_ops.add(parsedUrl.query, response)
+  }
+  else if (parsedUrl.pathname === "/resta") {
+    math_ops.substract(parsedUrl.query, response);
+  }
+  else{
+    response.write("no es suma ni resta");
   }
 })
 
-/**
- ** isNumeric returns true if input is a number. This can be done using the oppsite behaviour from isNaN function
- **/
-function isNumeric(num) {
-  //isNaN returns false if the input is a number, true otherwise
-  return !isNaN(num)
-}
-
-server.listen(8080)
+server.listen(9000)
